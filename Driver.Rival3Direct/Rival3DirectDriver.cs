b22cb7e9ab20764997ba7ad7d9ff2fa7a9906578
Rafee <rafeesamith@gmail.com>
2022-03-02 17:02:47 +0300

    First build, single LED and seizure-inducing

--- Driver.Rival3Direct/Rival3DirectDriver.cs
+++ Driver.Rival3Direct/Rival3DirectDriver.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimpleLed;
+using Rival3Support;
 
 namespace Driver.Rival3Direct
 {
     public class Rival3DirectDriver : ISimpleLed
     {
+        Rival3Controller r3controller;
+
         public event Events.DeviceChangeEventHandler DeviceAdded;
         public event Events.DeviceChangeEventHandler DeviceRemoved;
+
 
         public void Configure(DriverDetails driverDetails)
         {
@@ -21,7 +25,7 @@ namespace Driver.Rival3Direct
             }
         }
 
-        ControlDevice.LedUnit[] leds = new ControlDevice.LedUnit[4];
+        ControlDevice.LedUnit[] leds = new ControlDevice.LedUnit[1];
         public List<ControlDevice> GetDevices()
         {
             return new List<ControlDevice>
@@ -49,7 +53,19 @@ namespace Driver.Rival3Direct
 
         public DriverProperties GetProperties()
         {
-            throw new NotImplementedException();
+            return new DriverProperties
+            {
+                SupportsPush = true,
+                SupportsPull = false,
+                IsSource = false,
+                SupportsCustomConfig = false,
+                Id = Guid.Parse("afeeea5d-49b9-43dd-bb66-dfd20c1c3751"),
+                Author = "Rafee",
+                Blurb = "My first attempt at making a driver, for direct control of the Steelseries Rival 3",
+                CurrentVersion = new ReleaseNumber("1.0.0.6"),
+                IsPublicRelease = false,
+                GitHubLink = "https://github.com/RafeeSamith/Driver.Rival3Direct"
+            };
         }
 
         public void InterestedUSBChange(int VID, int PID, bool connected)
@@ -62,6 +78,28 @@ namespace Driver.Rival3Direct
             return "Steelseries Rival 3 Direct";
         }
 
+        public class Rival3LEDData : ControlDevice.LEDData
+        {
+
+            public float R { get; set; }
+            public float G { get; set; }
+            public float B { get; set; }
+        }
+
+        public Rival3DirectDriver()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                leds[i] = new ControlDevice.LedUnit
+                {
+                    LEDName = "LED " + i,
+                    Data = new Rival3LEDData
+                    {
+                        LEDNumber = i,
+                    },
+                };
+            }
+        }
         public void Pull(ControlDevice controlDevice)
         {
             throw new NotImplementedException();
@@ -69,7 +107,17 @@ namespace Driver.Rival3Direct
 
         public void Push(ControlDevice controlDevice)
         {
-            throw new NotImplementedException();
+            r3controller = new Rival3Controller();
+            r3controller.packets(255, 0, 0);
+            for (int i = 0; i < 1; i++)
+            {
+                byte r = (byte)controlDevice.LEDs[i].Color.Red;
+                byte g = (byte)controlDevice.LEDs[i].Color.Green;
+                byte b = (byte)controlDevice.LEDs[i].Color.Blue;
+                r3controller.packets(r, g, b);
+            }
+            //r3controller = new Rival3Controller();
+            //r3controller.packets(255, 25, 0);
         }
 
         public void PutConfig<T>(T config) where T : SLSConfigData
---
+++ Driver.Rival3Direct/Rival3Support.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using HidSharp;
+using System.Diagnostics;
+using SimpleLed;
+
+namespace Rival3Support
+{
+    public class Rival3Controller
+    {
+        HidDevice device = null;
+        HidStream stream = null;
+
+        //Steelseries Rival 3 (Updated Firmware) IDs
+        public int vendorID = 0x1038;
+        public int productID = 0x184C;
+
+        public Rival3Controller()
+        {
+
+        }
+
+        //Function to send a given packet to the device
+        public byte[] send_packet(byte[] packet)
+        {
+            this.stream.Write(packet);
+            return packet;
+        }
+
+        //Functions that processes the SimpleLED data into a packet
+        public void packets(byte r, byte g, byte b)
+        {
+            var terp = new OpenConfiguration();
+            terp.SetOption(OpenOption.Exclusive, true);
+            var list = DeviceList.Local;
+            var devices = list.GetHidDevices(vendorID, productID);
+
+            byte[] pkt = new byte[16];
+            //Prefix packets for individual RGB control
+            pkt[0] = 0x00;
+            pkt[1] = 0x0a;
+            pkt[2] = 0x00;
+            pkt[3] = 0x0f;
+
+            //Convert R, G, B values to Rival 3 protocol format
+            for (int i = 1; i <= 4; i++)
+            {
+                pkt[3*i+1] = r;
+                pkt[3*i+2] = g;
+                pkt[3*i+3] = b;
+            }
+
+            //Pick correct interface and send packet
+            foreach (Device device in devices)
+            {
+                //Interface 3, please let me know if there is a better way to do this
+                if (device.DevicePath.IndexOf("mi_03") != -1)
+                {
+                    stream = (HidStream)device.Open();
+                    stream.Write(pkt);
+                }
+            }
+        }
+
+    }
+   
+}
